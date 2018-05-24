b94cb7d5aeadb6eefbfd85364dcc736b1a76aa4b
Hkaur7 <hkaur7@seasiainfotech.com>
2018-05-24 14:21:27 +0530

    New Submit
    New

---
+++ src/test/java/mytest/Test2.java
@@ -0,0 +1,27 @@
+package mytest;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Test2 {
+
+	WebDriver driver;
+	 @Test 
+	    public void testSomeLibraryMethod_44() throws InterruptedException {
+	       // Library classUnderTest = new Library();
+	        System.out.println("***********************Hello Harinder_4*********************");
+	        System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "/BrowserExecutables/chromedriver_win32.exe");
+			System.out.println("Google chrome selected");
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--start-maximized");
+	        
+	        driver= new ChromeDriver();
+	        driver.get("https://www.google.com/");
+	        Thread.sleep(3000);
+	        driver.quit();
+	       // Assert.assertEquals("rwr", "545");
+	    }
+}
