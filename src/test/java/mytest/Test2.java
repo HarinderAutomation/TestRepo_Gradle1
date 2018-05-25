91790095b1de3f5e380519c6f60c28998ce3b81f
Hkaur7 <hkaur7@seasiainfotech.com>
2018-05-25 12:04:25 +0530

    kl
    kl

--- src/test/java/mytest/Test2.java
+++ src/test/java/mytest/Test2.java
@@ -9,7 +9,7 @@ public class Test2 {
 
 	WebDriver driver;
 	 @Test 
-	    public void testSomeLibraryMethod_45() throws InterruptedException {
+	    public void testSomeLibraryMethod_4() throws InterruptedException {
 	       // Library classUnderTest = new Library();
 	        System.out.println("***********************Hello Harinder_4*********************");
 	        System.setProperty("webdriver.chrome.driver",
